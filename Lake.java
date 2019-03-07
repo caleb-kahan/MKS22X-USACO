782c10fff8ff8450c23a858284471e253c800986
Caleb Kahan <ckahan00@stuy.edu>
2019-03-07 18:59:05 +0000

    everything initialized

--- Lake.java
+++ Lake.java
@@ -15,14 +15,16 @@ public class Lake{
       int i = 1;
       int row =0;
       int col =0;
+      pasture=new int[row][col];
       while(sys.hasNextInt()){
         int num = sys.nextInt();
         if(i==1)row=num;
         if(i==2)col=num;
         if(i==3)lakeElevation=num;
-        if(i==4)stompInstructions = new int [num];
+        if(i==4)stompInstructions = new int [num][3];
+	if(i>=5) pasture = new int[row][col];
         if(i>=5 && i<=row*col+4){
-          int actualColumn = (i--5) % col;
+          int actualColumn = (i-5) % col;
           int actualRow = (i-5) / col;
           pasture[actualRow][actualColumn]=num;
         }
