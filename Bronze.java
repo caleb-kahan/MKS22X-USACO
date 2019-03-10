4599c568950de4a132bd316b1c81d39f863b07d1
Caleb Kahan <ckahan00@stuy.edu>
2019-03-10 17:47:33 -0400

    Copied and cleaned

---
+++ Bronze.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Bronze{
+  public static void bronze(){
+    int [][] pasture=new int[0][0];
+    int [][] stompInstructions=new int[0][0];
+    int lakeElevation=initialize(pasture,stompInstructions,fileName);
+
+    }
+    public static int initialize(int [][] pasture, int [][] stompInstructions, String fileName){
+      try{
+        File file = new File(fileName);
+      }catch(FileNotFoundException e){
+        System.out.println("Invalid filename: "+fileName);
+      }
+      Scanner sys = new Scanner(file);
+      //Hope, I'm alloed to use delimeters
+      sys.useDelimiter("\\s*");
+        int i = 1;
+        int row =0;
+        int col =0;
+        pasture=new int[row][col];
+        while(sys.hasNextInt()){
+          int num = sys.nextInt();
+          if(i==1)row=num;
+          if(i==2)col=num;
+          if(i==3)lakeElevation=num;
+          if(i==4)stompInstructions = new int [num][3];
+  	      if(i>=5) pasture = new int[row][col];
+          if(i>=5 && i<=row*col+4){
+            int actualColumn = (i-5) % col;
+            int actualRow = (i-5) / col;
+            pasture[actualRow][actualColumn]=num;
+          }
+          if(i>row*col+4){
+  	         for(int j=0;j<3;j++){
+  	  	         stompInstructions[i-(row*col+5)][j]=num;
+  		           if(j!=2)num = sys.nextInt();
+  	         }
+          }
+  	      i++;
+      }
+   }
+   public void stomp(){
+     for(int i=0;i<stompInstructions.length;i++){
+       int [] stomper = stompInstructions[i];
+       int row = stomper[0]-1;
+       int col = stomper[1]-1;
+     }
+   }
+}
--- MakeLake.java
+++ MakeLake.java
@@ -1,5 +1,3 @@
-public class MakeLake{
-    public static void main(String [] args){
-      
-    }
+public class Bronze{
+  
 }
