16478291f346be9116c715c385c78edc0679fbdd
Caleb Kahan <ckahan00@stuy.edu>
2019-03-10 21:24:46 -0400

    I generate and I'm done

--- Silver.java
+++ Silver.java
@@ -1,21 +1,17 @@
 public class Silver{
   public static int silver(String fileName){
-    String [][] pasture= new String[0][0];
     int [][] optimize = new int [0][0];
     Integer time=0;
     Integer row1=0;
     Integer col1=0;
     Integer row2=0;
     Integer col2=0;
-    initialize(pasture,optimize,time,row1,col1,row2,col2,fileName);
+    initialize(optimize,time,row1,col1,row2,col2,fileName);
     if(! immediateCheck) return 0;
 
 
   }
-  public static int initialize(String [][] pasture, int[][] optimize,
-                               Integer time, Integer row1, Integer col1,
-                               Integer row2, Integer col2,String fileName){
-                                 
+  public static int initialize(int[][] optimize, Integer time, Integer row1, Integer col1, Integer row2, Integer col2,String fileName){
     try{
       File file = new File(fileName);
     }catch(FileNotFoundException e){
@@ -26,25 +22,30 @@ public class Silver{
     sys.useDelimiter("\\s*");
     int row = sys.nextInt();
     int col = sys.nextInt();
-    pasture = new int[row][col];
+    optimize = new int[row][col];
     time = sys.nextInt();
     for(int i=0;i<row;i++){
       for(int j=0;j<col;j++){
-        pasture[i][j]=sys.next();
+        if(sys.next()=="*")
+          pasture[i][j]=-1;
       }
     }
     row1=sys.nextInt()-1;
     col1=sys.nextInt()-1;
     row2=sys.nextInt()-1;
     col2=sys.nextInt()-1;
-    pasture[row1][col2]=1;
+    pasture[row1][col1]=1;
   }
   public static boolean immediateCheck(int r1, int c1, int r2, int c2, int time){
     boolean oddTime = time%2==1;
     boolean oddDistance = (Math.abs(r2-r1)+Math.abs(c2-r2))%2==1;
     return oddTime == oddDistance;
   }
-  public static boolean generate(int[][]pasture){
-
+  public static void generate(int[][]optimize){
+    for(int i=0;i<optimze.length;i++){
+      for(int j=0;j<optimize[0].length;j++){
+        
+      }
+    }
   }
 }
