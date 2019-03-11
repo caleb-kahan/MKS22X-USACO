56a9f1a38f98e78d175859c361c58d0068ee8a3a
Caleb Kahan <ckahan00@stuy.edu>
2019-03-11 14:44:07 +0000

    works like a baby, split worked

--- Silver.java
+++ Silver.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class Silver{
   public static int silver(String fileName){
@@ -18,20 +17,17 @@ public class Silver{
     rowsColsTime = generateRowsColsTime(sys);
     optimize = generateOptimize(rowsColsTime[0],rowsColsTime[1],sys);
     canMove = new boolean[rowsColsTime[0]][rowsColsTime[1]];
-    /*startEnd = generateStartEnd(sys);
+    startEnd = generateStartEnd(sys);
     if(! immediateCheck(startEnd[0],startEnd[1],startEnd[2],startEnd[3],rowsColsTime[2])) return 0;
     optimize[startEnd[0]][startEnd[1]]=1;
     for(int i=1;i<=rowsColsTime[2];i++){
-      //printer(optimize);
-      //System.out.println();
       canMove = generateCanMove(i,startEnd[0], startEnd[1],canMove);
       optimize=generate(optimize,canMove);
     }
     sys.close();
-    return optimize[startEnd[2]][startEnd[3]];*/
-    return 0;
+    return optimize[startEnd[2]][startEnd[3]];
   }
-  public static void printer(int [][] optimize){
+  private static void printer(int [][] optimize){
 	for(int i=0;i<optimize.length;i++){
 		for(int j=0;j<optimize[0].length;j++){
 			System.out.print(optimize[i][j]+" ");
@@ -41,7 +37,7 @@ public class Silver{
     }
   public static int [] generateRowsColsTime(Scanner sys){
     //Hope, I'm alloed to use delimeters
-    sys.useDelimiter("\\s*");
+    //sys.useDelimiter("\\s*");
     String toBeAnalyzed = sys.nextLine();
     String [] analyzer = toBeAnalyzed.split(" ");
     int [] returner = new int[3];
@@ -53,9 +49,10 @@ public class Silver{
   public static int [][] generateOptimize(int rows, int cols, Scanner sys){
 	int [][] optimize = new int[rows][cols];
 	for(int i=0;i<rows;i++){
+		String str = sys.nextLine();
+		String [] values = str.split("");
       		for(int j=0;j<cols;j++){
-			String str = sys.next();
-        		if(str.equals("*")){
+        		if(values[j].equals("*")){
           			optimize[i][j]=-1;
 			}
 		}
@@ -76,21 +73,11 @@ public class Silver{
   public static int [] generateStartEnd(Scanner sys){
 	String toBeAnalyzed = sys.nextLine();
     	String [] analyzer = toBeAnalyzed.split(" ");
-    	int [] returner = new int[3];
-    	returner[0]=Integer.parseInt(analyzer[0]);
-    	returner[1]=Integer.parseInt(analyzer[1]);
-    	returner[2]=Integer.parseInt(analyzer[2]);
-	int row1=Integer.parseInt(sys.next(Pattern.compile("\\S*")))-1;
-	//System.out.println(row1);
-    	int col1=Integer.parseInt(sys.next(Pattern.compile("\\S*")))-1;
-	//System.out.println(col1);
-    	int row2=Integer.parseInt(sys.next(Pattern.compile("\\d*")))-1;
-    	int col2=Integer.parseInt(sys.next(Pattern.compile("\\d*")))-1;
-	int [] returner = new int[4];
-	returner[0]=row1;
-	returner[1]=col1;
-	returner[2]=row2;
-	returner[3]=col2;
+    	int [] returner = new int[4];
+    	returner[0]=Integer.parseInt(analyzer[0])-1;
+    	returner[1]=Integer.parseInt(analyzer[1])-1;
+    	returner[2]=Integer.parseInt(analyzer[2])-1;
+	returner[3]=Integer.parseInt(analyzer[3])-1;
 	return returner;
    }
 	
--- SilverDriver.java
+++ SilverDriver.java
@@ -1,6 +1,6 @@
 public class SilverDriver{
   public static void main(String [] args){
-    //System.out.println(Silver.silver("ctravel.1.in"));
+    System.out.println(Silver.silver("ctravel.1.in"));
     System.out.println(Silver.silver("ctravel.2.in"));
     System.out.println(Silver.silver("ctravel.3.in"));
     System.out.println(Silver.silver("ctravel.4.in"));
