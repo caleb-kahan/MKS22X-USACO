a512e56e9b977a97d3950c830de68d0fb967b180
Caleb Kahan <ckahan00@stuy.edu>
2019-03-11 15:00:58 +0000

    basically silver but with a few edits

--- Bronze.java
+++ Bronze.java
@@ -3,18 +3,60 @@ import java.util.*;
 
 public class Bronze{
   public static int bronze(String fileName){
-    int [][] pasture=new int[0][0];
-    int [][] stompInstructions=new int[0][0];
-    int lakeElevation=0;
+    int [] rowsColsElevationStomping = new int [4];
+    int [][] pasture;
+    int [][] stompInstructions;
+    Scanner sys=new Scanner("");
     try{
-      lakeElevation=initialize(pasture,stompInstructions,fileName);
+	File file = new File(fileName);
+	sys = new Scanner(file);
     }catch(FileNotFoundException e){
-        System.out.println("Invalid filename: "+fileName);
+      System.out.println("Invalid filename: "+fileName);
     }
-    stomp(stompInstructions, pasture);
-    return makeALakeAndCalculateVolume(pasture,lakeElevation);
+    rowsColsElevationStomping = generateRowsColsElevationStomping(sys);
+    pasture=generatePasture(rowsColsEelevationStomping[0],rowsColsElevationStomping[1],sys);
+    stompInstructions = generateStompInstructions(rowsColsElevationStomping[3],sys);
+    pasture = stomp(stompInstructions, pasture);
+    return makeALakeAndCalculateVolume(pasture,rowsColsElevationStomping[2]);
     }
-    public static int initialize(int [][] pasture, int [][] stompInstructions, String fileName)throws FileNotFoundException{
+    public static int [] generateRowsColsElevationStomping(Scanner sys){
+	String toBeAnalyzed = sys.nextLine();
+    	String [] analyzer = toBeAnalyzed.split(" ");
+    	int [] returner = new int[4];
+    	returner[0]=Integer.parseInt(analyzer[0]);
+    	returner[1]=Integer.parseInt(analyzer[1]);
+    	returner[2]=Integer.parseInt(analyzer[2]);
+	returner[3]=Integer.parseInt(analyzer[3]);
+	return returner;
+    }
+    public static int [][] generatePasture(int rows, int cols, Scanner sys){
+	int [][] pasture = new int[rows][cols]
+	for(int i=0;i<rows;i++){
+		String str = sys.nextLine();
+		String [] values = str.split(" ");
+      		for(int j=0;j<cols;j++){
+          		pasture[i][j]=values[j];
+		}
+	}
+	return optimize;
+    }
+    public static int[][] generateStompInstructions(int times,sys){
+	int [][] stompInstructions = new int[times][3];
+	for(int i=0;i<times;i++){
+		String toBeAnalyzed = sys.nextLine();
+    		String [] analyzer = toBeAnalyzed.split(" ");
+		for(int j=0;j<3;j++){
+			int [] stompInstruction = new int[3];
+    			stompInstruction[0]=Integer.parseInt(analyzer[0]);
+    			stompInstruction[1]=Integer.parseInt(analyzer[1]);
+    			stompInstruction[2]=Integer.parseInt(analyzer[2]);
+		}
+	}
+	return stompInstuctions;
+     }
+		
+		
+    public static int initialize(int [][] pasture, int [][] stompInstructions, String fileName){
       int lakeElevation=0;
       File file = new File(fileName);
       //Hope, I'm alloed to use delimeters
@@ -47,7 +89,7 @@ public class Bronze{
       return lakeElevation;
 
    }
-   public static void stomp(int [][] stompInstructions, int [][] pasture){
+   public static int [][] stomp(int [][] stompInstructions, int [][] pasture){
      for(int i=0;i<stompInstructions.length;i++){
        int [] stomper = stompInstructions[i];
        int row = stomper[0]-1;
@@ -66,6 +108,7 @@ public class Bronze{
          }
        }
      }
+     return pasture;
    }
    public static int makeALakeAndCalculateVolume(int [][] pasture, int lakeElevation){
      int combine =0;
