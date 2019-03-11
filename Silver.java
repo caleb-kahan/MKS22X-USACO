7e4e71b29b4ec2269f6041e01a076012a8c93812
Caleb Kahan <ckahan00@stuy.edu>
2019-03-10 21:21:31 -0400

    I just realized why do I even need pasture???

--- Silver.java
+++ Silver.java
@@ -1,16 +1,21 @@
 public class Silver{
   public static int silver(String fileName){
-    int [][] pasture= newint[0][0];
+    String [][] pasture= new String[0][0];
+    int [][] optimize = new int [0][0];
     Integer time=0;
     Integer row1=0;
     Integer col1=0;
     Integer row2=0;
     Integer col2=0;
-    initialize(pasture,time,row1,col1,row2,col2,fileName);
+    initialize(pasture,optimize,time,row1,col1,row2,col2,fileName);
     if(! immediateCheck) return 0;
 
+
   }
-  public static int initialize(int [][] pasture, Integer time, Integer row1, Integer col1, Integer row2, Integer col2,String fileName){
+  public static int initialize(String [][] pasture, int[][] optimize,
+                               Integer time, Integer row1, Integer col1,
+                               Integer row2, Integer col2,String fileName){
+                                 
     try{
       File file = new File(fileName);
     }catch(FileNotFoundException e){
@@ -28,14 +33,18 @@ public class Silver{
         pasture[i][j]=sys.next();
       }
     }
-    row1=sys.nextInt();
-    row2=sys.nextInt();
-    row3=sys.nextInt();
-    row4=sys.nextInt();
+    row1=sys.nextInt()-1;
+    col1=sys.nextInt()-1;
+    row2=sys.nextInt()-1;
+    col2=sys.nextInt()-1;
+    pasture[row1][col2]=1;
   }
   public static boolean immediateCheck(int r1, int c1, int r2, int c2, int time){
     boolean oddTime = time%2==1;
     boolean oddDistance = (Math.abs(r2-r1)+Math.abs(c2-r2))%2==1;
     return oddTime == oddDistance;
+  }
+  public static boolean generate(int[][]pasture){
+
   }
 }
