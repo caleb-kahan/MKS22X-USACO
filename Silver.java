e206181156a1bcd47ef0c7092e6268eeb58ec6f8
Caleb Kahan <ckahan00@stuy.edu>
2019-03-10 21:59:57 -0400

    input-mismatch

--- Silver.java
+++ Silver.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Silver{
   public static int silver(String fileName){
     int [][] optimize = new int [0][0];
@@ -6,19 +9,19 @@ public class Silver{
     Integer col1=0;
     Integer row2=0;
     Integer col2=0;
-    initialize(optimize,time,row1,col1,row2,col2,fileName);
-    if(! immediateCheck) return 0;
+    try{
+      initialize(optimize,time,row1,col1,row2,col2,fileName);
+    }catch(FileNotFoundException e){
+      System.out.println("Invalid filename: "+fileName);
+    }
+    if(! immediateCheck(row1,col1,row2,col2,time)) return 0;
     for(int i=0;i<time;i++){
       generate(optimize);
     }
     return optimize[row2][col2];
   }
-  public static int initialize(int[][] optimize, Integer time, Integer row1, Integer col1, Integer row2, Integer col2,String fileName){
-    try{
-      File file = new File(fileName);
-    }catch(FileNotFoundException e){
-      System.out.println("Invalid filename: "+fileName);
-    }
+  public static void initialize(int[][] optimize, Integer time, Integer row1, Integer col1, Integer row2, Integer col2,String fileName)throws FileNotFoundException{
+    File file = new File(fileName);
     Scanner sys = new Scanner(fileName);
     //Hope, I'm alloed to use delimeters
     sys.useDelimiter("\\s*");
@@ -29,14 +32,14 @@ public class Silver{
     for(int i=0;i<row;i++){
       for(int j=0;j<col;j++){
         if(sys.next()=="*")
-          pasture[i][j]=-1;
+          optimize[i][j]=-1;
       }
     }
     row1=sys.nextInt()-1;
     col1=sys.nextInt()-1;
     row2=sys.nextInt()-1;
     col2=sys.nextInt()-1;
-    pasture[row1][col1]=1;
+    optimize[row1][col1]=1;
   }
   public static boolean immediateCheck(int r1, int c1, int r2, int c2, int time){
     boolean oddTime = time%2==1;
@@ -44,9 +47,9 @@ public class Silver{
     return oddTime == oddDistance;
   }
   public static void generate(int[][]optimize){
-    for(int i=0;i<optimze.length;i++){
+    for(int i=0;i<optimize.length;i++){
       for(int j=0;j<optimize[0].length;j++){
-        optimzie[i][j]+=checker(optimize,i,j);
+        optimize[i][j]+=checker(optimize,i,j);
       }
     }
   }
